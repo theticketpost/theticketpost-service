81292957c0903385c7baec808a950336e204989c
David Antúnez <dantunezglez@gmail.com>
2022-05-06 10:32:04 +0200

    basic python3 project structure

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # theticketpost-service
+
+## How to install
+```
+python3 setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="theticketpost-service",
+    version="0.0.1",
+    description="A custom newspaper everyday",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theticketpost/theticketpost-service",
+    author="David Antunez, Bruno Cabado",
+    author_email="@eipporko, @brunocabado",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    entry_points={"console_scripts": ["theticketpost-service = src.app:main"]},
+    python_requires=">=3.7",
+)

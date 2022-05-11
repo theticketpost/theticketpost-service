846b25c79bca78ebaa12b8d583873e0a008b28d8
David Antúnez <dantunezglez@gmail.com>
2022-05-12 00:34:58 +0200

    Adding sections

--- src/app.py
+++ src/app.py
@@ -2,12 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+version = '0.0.1'
+
+
 @app.route('/')
 @app.route('/index')
-def hello():
+@app.route('/dashboard')
+def dashboard():
     name = 'Motherfucker'
     version = '0.0.1'
-    return render_template('hello.html', title='TheTicketPost', version=version, username=name)
+    return render_template('dashboard.html', title='TheTicketPost', version=version, username=name)
+
+
+@app.route('/apps')
+def apps():
+    return render_template('apps.html', title='TheTicketPost', version=version)
+
+
+@app.route('/store')
+def store():
+    return render_template('store.html', title='TheTicketPost', version=version)
+
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html', title='TheTicketPost', version=version)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='TheTicketPost', version=version)
+
 
 def main():
     app.run( host='0.0.0.0', port=8080 )
